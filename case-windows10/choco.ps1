cb2fddfe2de623c5a20c9b634bfb6ab65ab1008d
Nicolas Yurchuk <nicolas.yurchuk@gmail.com>
2020-08-09 23:30:34 +0300

    09.08.20

--- case-windows10/choco.ps1
+++ case-windows10/choco.ps1
@@ -40,6 +40,7 @@ do
     "googlechrome" "Google Chrome" "Google Chrome" \ `
     "vlc" "VLC media player" "VLC Media Player" \ `
     "ccleaner" "CCleaner" "The number-one tool for cleaning your Windows PC" \ `
+    "calibre" "Calibre" "One stop solution to all your e-book" \ `
     "libreoffice-fresh" "Libre Office" "Free power-packed Open Source personal productivity suite" \ `
     "far" "FAR Manager" "File and ARchive Manager" \ `
     "notepadplusplus" "Notepad++" "Notepad++ is a free source code editor" \ `
@@ -47,8 +48,12 @@ do
 	"vivaldi" "Vivaldi" "Vivaldi" \ `
 	"paint.net" "Paint.net" "Image and photo manipulation software" \ `
 	"greenshot" "GreenShot" "Light-weight screenshot software tool" \ `
+	"rufus" "Rufus" "Create bootable USB drives from Windows and Linux images" \ `
 	"anydesk.install" "Anydesk" "Remote desktop software" \ `
 	"office365business" "Office 365" "Office 365 Business" \ `
+	"smartgit" "SmartGit" "Git client with support for GitHub Pull Requests+Comments, SVN and Mercurial" \ `
+	"nagstamon" "Nagstamon" "Nagstamon is a status monitor for the desktop" \ `
+	"innosetup" "Inno Setup" "Free installer for Windows programs" \ `
 	"powertoys" "PowerToys" "Windows system utilities to maximize productivity" \ `
 	"sysinternals" "Sysinternals Suite" "Troubleshooting Utilities have been rolled up into a single suite of tools." \ `
 	"pidgin" "Pidgin" "Chat program which lets you log into accounts on multiple chat networks simultaneously" \ `
@@ -64,6 +69,15 @@ do
 	"fsviewer" "FastStone Image Viewer" "Fast, stable, user-friendly image browser, converter and editor" \ `
 	"steam" "Steam" "Steam Client by Valve Corporation" \ `
 	"microsoft-teams.install" "Microsoft Teams" "Helps your team work better together" \ `
+	"stellarium" "Stellarium" "Software which renders realistic skies in real time" \ `
+	"linux-reader" "DiskInternals Linux Reader" "Access files and folders on Ext, UFS, HFS, ReiserFS, or APFS file systems from Windows" \ `
+	"powerbi" "Power BI Desktop" "Create stunning reports and visualizations with Power BI Desktop" \ `
+	"vmware-horizon-client" "VMWare Horizon Client" "Client application used to connect to virtual desktops from Windows desktop computers" \ `
+	"xmind" "XMind" "XMind - Powerful Mind Mapping Software" \ `
+	"phonerlite" "PhonerLite" "Easy to use VoIP softphone with profile and contact management" \ `
+	"victoria" "Victoria HDD-SSD" "HDD/SSD diagnostics, research, testing program" \ `
+	"amd-ryzen-chipset" "AMD Ryzen Chipset Drivers" "" \ `
+	"tightvnc" "TightVNC" "Remote control software" \ `
 	"doublecmd" "Double Commander" "Cross platform open source file manager with two panels side by side") -y
     } '3' {
     choco outdated
@@ -78,6 +92,7 @@ do
     "googlechrome" "Google Chrome" "Google Chrome" \ `
     "vlc" "VLC media player" "VLC Media Player" \ `
     "ccleaner" "CCleaner" "The number-one tool for cleaning your Windows PC" \ `
+    "calibre" "Calibre" "One stop solution to all your e-book" \ `
     "libreoffice-fresh" "Libre Office" "Free power-packed Open Source personal productivity suite" \ `
     "far" "FAR Manager" "File and ARchive Manager" \ `
     "notepadplusplus" "Notepad++" "Notepad++ is a free source code editor" \ `
@@ -85,8 +100,12 @@ do
 	"vivaldi" "Vivaldi" "Vivaldi" \ `
 	"paint.net" "Paint.net" "Image and photo manipulation software" \ `
 	"greenshot" "GreenShot" "Light-weight screenshot software tool" \ `
+	"rufus" "Rufus" "Create bootable USB drives from Windows and Linux images" \ `
 	"anydesk.install" "Anydesk" "Remote desktop software" \ `
 	"office365business" "Office 365" "Office 365 Business" \ `
+	"smartgit" "SmartGit" "Git client with support for GitHub Pull Requests+Comments, SVN and Mercurial" \ `
+	"nagstamon" "Nagstamon" "Nagstamon is a status monitor for the desktop" \ `
+	"innosetup" "Inno Setup" "Free installer for Windows programs" \ `
 	"powertoys" "PowerToys" "Windows system utilities to maximize productivity" \ `
 	"sysinternals" "Sysinternals Suite" "Troubleshooting Utilities have been rolled up into a single suite of tools." \ `
 	"pidgin" "Pidgin" "Chat program which lets you log into accounts on multiple chat networks simultaneously" \ `
@@ -102,6 +121,15 @@ do
 	"fsviewer" "FastStone Image Viewer" "Fast, stable, user-friendly image browser, converter and editor" \ `
 	"steam" "Steam" "Steam Client by Valve Corporation" \ `
 	"microsoft-teams.install" "Microsoft Teams" "Helps your team work better together" \ `
+	"stellarium" "Stellarium" "Software which renders realistic skies in real time" \ `
+	"linux-reader" "DiskInternals Linux Reader" "Access files and folders on Ext, UFS, HFS, ReiserFS, or APFS file systems from Windows" \ `
+	"powerbi" "Power BI Desktop" "Create stunning reports and visualizations with Power BI Desktop" \ `
+	"vmware-horizon-client" "VMWare Horizon Client" "Client application used to connect to virtual desktops from Windows desktop computers" \ `
+	"xmind" "XMind" "XMind - Powerful Mind Mapping Software" \ `
+	"phonerlite" "PhonerLite" "Easy to use VoIP softphone with profile and contact management" \ `
+	"victoria" "Victoria HDD-SSD" "HDD/SSD diagnostics, research, testing program" \ `
+	"amd-ryzen-chipset" "AMD Ryzen Chipset Drivers" "" \ `
+	"tightvnc" "TightVNC" "Remote control software" \ `
 	"doublecmd" "Double Commander" "Cross platform open source file manager with two panels side by side") -y
     } '6' {
     powercfg -h off
