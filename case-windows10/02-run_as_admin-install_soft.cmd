2d5d8d56719297c85e1e43aaad852006748ee70a
nyurch <nicolas.yurchuk@gmail.com>
2021-03-06 14:49:28 +0200

    Update 02-run_as_admin-install_soft.cmd

--- case-windows10/02-run_as_admin-install_soft.cmd
+++ case-windows10/02-run_as_admin-install_soft.cmd
@@ -1,7 +1,6 @@
 @echo off
 cls
 color FC
-cd %~dp0
 net session >nul 2>&1
-if %errorLevel% == 0 (powershell -executionpolicy RemoteSigned -file choco.ps1) else (echo "Run as Administrator please...")
+if %errorLevel% == 0 (powershell -executionpolicy RemoteSigned -file %~dp0\choco.ps1) else (echo "Run as Administrator please...")
 pause
